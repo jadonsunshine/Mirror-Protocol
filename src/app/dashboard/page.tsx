74106676bb201140431a7d10cd5c7d2489c4b347
jadonamite <jadonamite@gmail.com>
2026-03-28 20:39:28 +0100

    feat: implement ui logic (cleanup)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { Footer } from "@/components/fundx/Footer"
 import { useStacks } from "@/components/fundx/StacksProvider"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Button } from "@/components/ui/button"
+import { CreatorTab } from "@/components/dashboard/CreatorTab"
 
 
 export default function DashboardPage() {
